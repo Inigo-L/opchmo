91709fe31b8a7feb2612a1643319a73e703d2247
HMellor <hej.mellor@gmail.com>
2020-05-07 20:56:40 +0100

    Small update because app was duplicated

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAwIaQ8PdDVuoOwKULFLwrgb945OndQr80",
   authDomain: "opchmo.firebaseapp.com",
   databaseURL: "https://opchmo.firebaseio.com",
   projectId: "opchmo",
   storageBucket: "opchmo.appspot.com",
   messagingSenderId: "78441034334",
-  appId: "1:78441034334:web:b3d881fedacbbc7a6cfacf"
+  appId: "1:78441034334:web:6581f04a5878842a6cfacf"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
